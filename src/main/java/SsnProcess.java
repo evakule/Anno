f6bebd72bed74caeafbb8eaca8ce18e21f9164ae
egor.vakulenko <evakule15@gmail.com>
2020-05-04 11:13:13 +0300

    Updating anonymizing strategy

---
+++ src/main/java/SsnProcess.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class SsnProcess {
+    private static final String FILE_PATH = "src/main/java/prerequisites/tn_ssn4_lookup.csv";
+    private final HashMap<String, String> ssnList = new HashMap<>();
+
+    public SsnProcess() {
+        String row;
+        try (
+                BufferedReader csvReader = new BufferedReader(new FileReader(FILE_PATH));
+        ) {
+            while ((row = csvReader.readLine()) != null) {
+                String[] dividedRow = row.split(",");
+                ssnList.put(dividedRow[1], dividedRow[0]);
+                System.out.println(dividedRow[1] + "_" + dividedRow[0]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<String, String> getSsnList() {
+        return ssnList;
+    }
+}
---
+++ src/main/java/util/RandomOrderAbcGenerator.java
@@ -0,0 +1,31 @@
+package util;
+
+public class RandomStringGenerator {
+    public static void main(String[] args) {
+        String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int stringRange = s.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < stringRange; i++) {
+            int randomNumber = getRandomIntegerBetweenRange(0, s.length() - 1);
+            char charFromString = s.charAt(randomNumber);
+            sb.append(charFromString);
+            s = removeCharFromString(s, charFromString);
+        }
+        System.out.println(sb.toString().toLowerCase());
+        System.out.println(sb.toString());
+    }
+
+    public static int getRandomIntegerBetweenRange(double min, double max) {
+        return (int) ((int) (Math.random() * ((max - min) + 1)) + min);
+    }
+
+    private static String removeCharFromString(String s, char c) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != c) {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
