70539709d3165a138ab6c936c9bdae29c186f5c5
egor.vakulenko <evakule15@gmail.com>
2020-05-14 21:15:41 +0300

    delimiter changing

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Runner {
-    private static final String FILE_PATH = "C:/Users/egor.vakulenko/Desktop/Players.csv";
+    private static final String FILE_PATH = "C:/Users/egor.vakulenko/Desktop/TestData.csv";
     private static final String NEW_FILE_PATH = "C:/Users/egor.vakulenko/Desktop/Players_anonymized.csv";
 
     public static void main(String[] args) {
@@ -20,12 +20,17 @@ public class Runner {
                 FileWriter csvWriter = new FileWriter(NEW_FILE_PATH)
         ) {
             while ((row = csvReader.readLine()) != null) {
+
                 if (counter > 0) {
-                    String[] rowColumns = row.split(";");
+                    String[] rowColumns = row.split(",");
                     String[] changedRow = encryption.getChangedRow(rowColumns);
                     row = build(changedRow);
+                    csvWriter.append(row).append("\n");
+                } else {
+                    String[] firstRow = row.split(",");
+                    row = build(firstRow);
+                    csvWriter.append(row).append("\n");
                 }
-                csvWriter.append(row).append("\n");
                 counter++;
             }
         } catch (IOException e) {
@@ -37,7 +42,7 @@ public class Runner {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < rowColumns.length; i++) {
             builder.append(rowColumns[i]);
-            builder.append(";");
+            builder.append(",");
         }
         return builder.toString();
     }
